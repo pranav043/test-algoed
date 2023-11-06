9c5f430d0898b57b490a3bc2885bdcc195f4c603
pranav043 <gupta.pranav043@gmail.com>
2023-11-07 01:54:39 +0530

    user fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,11 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
-const users = [
-  {
-    username: "pranav",
-    password: "$2b$10$YvDT1.W8.uD/zd16Gpv4c.T9b7mrGqHOeF20U5YmWlYnaDIbNXqV.", // Hashed password for 'pranav'
-  },
-];
+const users = require("../util/user");
 
 const authFunc = async (req, res, next) => {
   const { username, password } = req.body;
 
+  //Sample User List instead of creating User Table in DB
   const user = users.find((u) => u.username === username);
 
   if (!user || !bcrypt.compareSync(password, user.password)) {
---
+++ src/util/user.js
@@ -0,0 +1,8 @@
+const users = [
+  {
+    username: "pranav",
+    password: "$2b$10$YvDT1.W8.uD/zd16Gpv4c.T9b7mrGqHOeF20U5YmWlYnaDIbNXqV.", // Hashed password for 'pranav'
+  },
+];
+
+module.exports = users;
